ba187debe9ec576d4565a47fc06a4090b6f79615
kyawlinnthant <kyawlinnthant@ilbcedu.com>
2024-02-08 14:59:33 +0630

    update: role base restrict route

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,6 +16,8 @@ exports.signup = catchAsync(async (req, res, next) => {
         email: req.body.email,
         password: req.body.password,
         passwordConfirm: req.body.passwordConfirm,
+        passwordChangedAt: req.body.passwordChangedAt,
+        role: req.body.role,
     })
 
     const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
@@ -36,7 +38,6 @@ exports.login = catchAsync(async (req, res, next) => {
         return next(new AppError('Please provide email and password!', 400))
     }
     const user = await User.findOne({ email }).select('+password')
-
     if (!user || !(await user.correctPassword(password, user.password))) {
         return next(new AppError('Incorrect email or password', 401))
     }
@@ -48,6 +49,7 @@ exports.login = catchAsync(async (req, res, next) => {
 })
 
 exports.protect = catchAsync(async (req, res, next) => {
+    // 1) Getting token and check of it's there
     let token
     if (
         req.headers.authorization &&
@@ -57,9 +59,55 @@ exports.protect = catchAsync(async (req, res, next) => {
     }
 
     if (!token) {
-        return next(new AppError('Unauthorize request', 401))
+        return next(
+            new AppError(
+                'You are not logged in! Please log in to get access.',
+                401
+            )
+        )
     }
+
+    // 2) Verification token
     const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET)
-    console.log(decoded)
+
+    // 3) Check if user still exists
+    const currentUser = await User.findById(decoded.id)
+    if (!currentUser) {
+        return next(
+            new AppError(
+                'The user belonging to this token does no longer exist.',
+                401
+            )
+        )
+    }
+
+    // 4) Check if user changed password after the token was issued
+    if (currentUser.changedPasswordAfter(decoded.iat)) {
+        return next(
+            new AppError(
+                'User recently changed password! Please log in again.',
+                401
+            )
+        )
+    }
+
+    // GRANT ACCESS TO PROTECTED ROUTE
+    req.user = currentUser
     next()
 })
+
+exports.restrictTo = (...roles) => {
+    return (req, res, next) => {
+        // roles ['admin', 'lead-guide']. role='user'
+        console.log(req.user.role, 'restrict role')
+        if (!roles.includes(req.user.role)) {
+            return next(
+                new AppError(
+                    'You do not have permission to perform this action',
+                    403
+                )
+            )
+        }
+        next()
+    }
+}
